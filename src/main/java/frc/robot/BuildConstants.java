77a411ffa684e5246d785dd24387b4011dac961a
Matt Bishop <matt@withinfocus.com>
2025-01-20 10:14:16 -0500

    Version bumps and Gradle setup for later

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_ReefScape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "c816be7ae41535a61e9cb17fbae80e8790d5272c";
-  public static final String GIT_DATE = "2025-01-06 19:08:04 EST";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "0e06cbb4c48f90d4fe4aaa0f9f3d25a358b2cd81";
+  public static final String GIT_DATE = "2025-01-07 19:43:00 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-07 17:50:13 EST";
-  public static final long BUILD_UNIX_TIME = 1736290213682L;
+  public static final String BUILD_DATE = "2025-01-20 10:11:44 EST";
+  public static final long BUILD_UNIX_TIME = 1737385904153L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
