06dc8379e3fb1503ad231b08cc045f77c8df7672
Aryan <52215513+importly@users.noreply.github.com>
2025-02-15 20:05:48 -0500

    decreased the force of movement massively

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -25,38 +25,7 @@ import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class Vision extends SubsystemBase {
   private static final double fieldBorderMargin = 0.5;
-  private final Pose3d[] cameraPoses =
-      new Pose3d[] {
-        // Front Left
-        new Pose3d(
-            new Translation3d(0.270, 0.334, 0.267), // Right camera translation (X, Y, Z)
-            new Rotation3d(0.0, Units.degreesToRadians(-12.63), Units.degreesToRadians(45))),
-        // Front Right
-        new Pose3d(
-            new Translation3d(0.270, -0.334, 0.267), // Left camera translation (X, Y, Z)
-            new Rotation3d(
-                0.0,
-                Units.degreesToRadians(-12.63),
-                Units.degreesToRadians(-45))), // in radians btw
-        // Back Left
-        new Pose3d(
-            new Translation3d(-0.27, 0.334, 0.267), // Right camera translation (X, Y, Z)
-            new Rotation3d(0.0, Units.degreesToRadians(-12.63), Units.degreesToRadians(45 + 90))),
-        // Back Right
-        new Pose3d(
-            new Translation3d(-0.27, -0.334, 0.267), // Left camera translation (X, Y, Z)
-            new Rotation3d(
-                0.0,
-                Units.degreesToRadians(-12.63),
-                Units.degreesToRadians(-45 - 90))), // in radians btw
-
-        // front_center
-        new Pose3d(
-            new Translation3d(
-                0.31, -0.03, Units.inchesToMeters(7.7)), // Left camera translation (X, Y, Z)
-            new Rotation3d(
-                0.0, Units.degreesToRadians(-13.5), Units.degreesToRadians(0))), // in radians btw
-      };
+  private static final Pose3d[] cameraPoses = CameraPoses.cameraPoses;
 
   private final PhotonCamera[] cameras;
   /* For shooting vs. path following in auto */
