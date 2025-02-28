399e120ef0e9f75ba5af796bdf97bbb93acc9891
darkpotatoo <darkpotatoben@gmail.com>
2025-02-28 17:36:10 -0500

    2-sensor update and cancellable centering/gripping process

--- src/main/java/frc/robot/subsystems/FlipperSubsystem.java
+++ src/main/java/frc/robot/subsystems/FlipperSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -20,24 +21,32 @@ public class FlipperSubsystem extends SubsystemBase {
       new Solenoid(Constants.SOLENOID_MODULE_TYPE, Constants.FLIPPER_SOLENOID_CHANNEL);
   private Solenoid coralCenterMechanism =
       new Solenoid(Constants.SOLENOID_MODULE_TYPE, Constants.CENTERER_SOLENOID_CHANNEL);
-  private DigitalInput coralDetector = new DigitalInput(Constants.CORAL_SENSOR_CHANNEL);
+  private DigitalInput coralDetector1 = new DigitalInput(Constants.CORAL_SENSOR_1_CHANNEL);
+  private DigitalInput coralDetector2 = new DigitalInput(Constants.CORAL_SENSOR_2_CHANNEL);
 
   /** Subsystem handling coral intake and dropping onto branches/level1. */
   public FlipperSubsystem() {}
 
   /**
-   * If the flipper thinks it has/had coral (aka it dropped to the reef or tried gripping), it opens
-   * the gripper. If the flipper doesn't think it has coral (right after recieving basically), it
-   * centers then grips the coral.
+   * If the system thinks it has coral (dropped to the reef or had coral detected by both sensors), it opens
+   * the gripper. <p>If the system doesn't think it has coral (after recieving from station), the coral centerer
+   * will run every 2 seconds until both sensors detect a coral. While doing this, the system will think that it
+   * has coral. <p>Can be cancelled by running the method again.
    */
   public void flipperHoldingState() {
     if (!believesHasCoral) {
-      coralCenterMechanism.setPulseDuration(0.5);
-      coralCenterMechanism.startPulse();
-      while (coralCenterMechanism.get()) {}
-      gripper.set(coralDetector.get());
       believesHasCoral = true;
-    } else if (believesHasCoral) {
+      // Repeat until the coral is detected on both sensors or until the user cancels gripping
+      while ((!coralDetector1.get() || !coralDetector2.get()) && believesHasCoral){
+        coralCenterMechanism.setPulseDuration(0.5);
+        coralCenterMechanism.startPulse();
+        Timer.delay(Constants.SECONDS_PER_CENTERING_ATTEMPT);
+      }
+      // When detected, toggle gripper
+      gripper.set(true);
+    }
+    else if (believesHasCoral) {
+      // Release gripper and stop trying to center the coral
       gripper.set(false);
       coralCenterMechanism.set(false);
       believesHasCoral = false;
@@ -62,10 +71,10 @@ public class FlipperSubsystem extends SubsystemBase {
   public void periodic() {
     Logger.recordOutput("Flipper/Gripper Is Closed", gripper.get());
     Logger.recordOutput("Flipper/Robot Thinks Has Coral", believesHasCoral);
-    Logger.recordOutput("Flipper/Coral Secured", (gripper.get() && coralDetector.get()));
+    Logger.recordOutput("Flipper/Coral Secured", (gripper.get() && (coralDetector1.get() && coralDetector2.get())));
     SmartDashboard.putBoolean("Gripper Closed", gripper.get());
     SmartDashboard.putBoolean("Thinks has coral", believesHasCoral);
-    SmartDashboard.putBoolean("Coral Secured and Gripped", (gripper.get() && coralDetector.get()));
+    SmartDashboard.putBoolean("Coral Secured and Gripped", (gripper.get() && (coralDetector1.get() && coralDetector2.get())));
   }
 
   @Override
