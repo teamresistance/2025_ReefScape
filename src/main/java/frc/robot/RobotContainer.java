e7998109b894573ba58d293f12da5bd5991688d8
Matt Bishop <matt@withinfocus.com>
2025-04-15 11:15:25 -0400

    Revert "feat: auto changes, new named commands, and AutoScoreAlgaeCommand"
    This reverts commit a46188773730715de3c130f7187dd77910c5b9d5.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -128,10 +128,6 @@ public class RobotContainer {
 
     NamedCommands.registerCommand(
         "autoScore", new DeferredCommand(() -> new AutoScoreCommand(reef, drive, elevator)));
-    NamedCommands.registerCommand(
-    "autoScoreAlgae", new DeferredCommand(() -> new AutoScoreAlgaeCommand(reef, drive, elevator, false)));
-    NamedCommands.registerCommand(
-    "autoScoreBoth", new DeferredCommand(() -> new AutoScoreAlgaeCommand(reef, drive, elevator, true)));
   }
 
   private LoggedDashboardChooser<Command> configureAutos() {
